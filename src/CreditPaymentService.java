d814d2ff333d45199574fd6c91193f364e976bbc
NatalyaZinger <nata.zinger@gmail.com>
2022-06-20 08:13:58 +0500

    fixed locals

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
-    double rate = 9.99;
 
     public double calculatePayment(double term, double amount) {
+        double rate = 9.99;
         double rateMonth = rate / 100 / 12;
         double payment = amount * (rateMonth + (rateMonth / (Math.pow(1 + rateMonth, term * 12) - 1)));
         return payment;
